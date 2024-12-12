4ff2e0dd0072422ada7411f78d5a955ee42e6175
gonza56d <gonza56d@gmail.com>
2024-12-12 09:51:08 -0300

    Separator for treesitter context

--- init.lua
+++ init.lua
@@ -361,6 +361,7 @@ require("treesitter-context").setup(
         max_lines = 0, -- Set to 0 to display context even for very large functions
         trim_scope = "outer", -- Removes lines that would push content off the window
         mode = "cursor", -- Show context based on cursor location, not just top line
+        separator = "-",
         patterns = {
             -- Specific patterns to ensure context in Python
             python = {

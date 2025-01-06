91679256c24a5f99f5d788d11c46062d25407a3f
gonza56d <gonza56d@gmail.com>
2025-01-06 11:22:22 -0300

    fix lualine

--- init.lua
+++ init.lua
@@ -147,7 +147,7 @@ require("lazy").setup(
                         sections = {
                             lualine_a = {'mode'},
                             lualine_b = {'progress', 'location'},
-                            lualine_c = {'filename'},
+                            lualine_c = {{'filename', path = 1}},
                             lualine_x = {'filetype'},
                             lualine_y = {'branch'},
                             lualine_z = {'diff', 'diagnosis'}
